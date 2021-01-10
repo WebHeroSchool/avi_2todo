a406cfea27fb6d0ea9a51c5474aee1dc61e97a97
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-10 18:29:22 +0300

    вывод значений в jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const flag = false;
+const count = 10;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ color: "yellow", fontSize: 10 + 20 }}>Lorem, ipsum dolor.</p>
+        <p>{count}</p>
+        <p>{50}</p>
+        <p>{undefined}</p>
+        <p>{null}</p>
+        <p>{true}</p>
+        <p>{false}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {flag ? "flag is true" : "flag is false"}
+
         </p>
-        <p>hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
