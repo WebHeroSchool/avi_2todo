2520c03b420d8718b6415a1822ab6178b67e94aa
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-12 23:42:51 +0300

    структура проекта

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const element = (<div>jjjj
- <a>go it</a>
-</div>);
-
-export { element };
---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const InputItem = () => (<div>InputItem</div>);
+
+
+export default InputItem;
