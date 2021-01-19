4a285753e64a98a8863b6e7a9e4b797ac45463d2
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-20 00:38:20 +0300

    поправила условие с == на ===

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
   onClickDone = id => {
     const newItemList =  this.state.items.map(item => {
       const newItem = { ...item };
-      if (item.id == id) {
+      if (item.id === id) {
         newItem.isDone = !item.isDone;
       }
       return newItem;
