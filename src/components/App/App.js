36f1e1d75d3150f7f48c9345061a5d91f56a671f
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-19 18:56:11 +0300

    рефакторинг файла App.js - компонента-функции в компонент-класс

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,32 +4,31 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles  from './App.module.css';
 
-
-const App = () => {
-
-  const items = [
-    {
-      value: 'Задача № 1',
-      isDone: true
-    },
-    {
-      value: 'Задача № 2',
-      isDone: false
-    },
-    {
-      value: 'Задача № 3',
-      isDone: true
-    }
-  ];
-
-  return (<div className={styles.wrapper}>
-    <h1 className={styles.title}>Список задач</h1>
-    <InputItem />
-    <ItemList items={items} />
-    <Footer count={5} />
-  </div>
-  );
-
-};
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Задача № 1',
+        isDone: false
+      },
+      {
+        value: 'Задача № 2',
+        isDone: false
+      },
+      {
+        value: 'Задача № 3',
+        isDone: false
+      }
+    ];
+  
+     return (<div className={styles.wrapper}>
+      <h1 className={styles.title}>Список задач</h1>
+      <InputItem />
+      <ItemList items={items} />
+      <Footer count={5} />
+    </div>
+    );
+  }
+}
 
 export default App;
